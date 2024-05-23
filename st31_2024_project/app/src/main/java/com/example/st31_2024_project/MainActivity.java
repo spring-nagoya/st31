6e87d285182c7eabba27d708ddcb7fdb8203005a
sikigasa <km3t1114@yahoo.co.jp>
2024-05-23 10:21:18 +0900

    add: 授業

--- st31_2024_project/app/src/main/java/com/example/st31_2024_project/MainActivity.java
+++ st31_2024_project/app/src/main/java/com/example/st31_2024_project/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.st31_2024_project;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView strView = findViewById(R.id.textV);
+        strView.setText("はろー");
     }
 }
