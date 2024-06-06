3eda9da0784fa734b68a312cf37469cd65b2fd0a
sikigasa <km3t1114@yahoo.co.jp>
2024-06-06 09:25:58 +0900

    add: 授業

--- st31_2024_project/app/src/main/java/com/example/st31_2024_project/MainActivity.java
+++ st31_2024_project/app/src/main/java/com/example/st31_2024_project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.st31_2024_project;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -26,18 +27,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         TextView strView = findViewById(R.id.textV);
         strView.setText("はろー");
+
+        //    匿名クラスを使った方法
+        Button button;
+        button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText edt = findViewById(R.id.edText);
+                Editable ed = edt.getText();
+                String strName = ed.toString();
+
+                TextView strView = findViewById(R.id.textV);
+                strView.setText(strName);
+
+//                課題No.2
+
+                EditText editText = findViewById(R.id.edText2);
+                String strText2 = editText.getText().toString();
+
+                Integer intText2 = Integer.valueOf(strText2);
+                
+            }
+
+        });
     }
 
     @Override
     public void onClick(View v) {
-        EditText edt = findViewById(R.id.edText);
-        Editable ed = edt.getText();
-        String strName = ed.toString();
+        if ( v.getId() == R.id.btn ) {
+            EditText edt = findViewById(R.id.edText);
+            Editable ed = edt.getText();
+            String strName = ed.toString();
 
-        TextView strView = findViewById(R.id.textV);
-        strView.setText(strName);
-    }
+            TextView strView = findViewById(R.id.textV);
+            strView.setText(strName);
+        } else if ( v.getId() == R.id.textV) {
 
+        }
+
+
+
+    
 //    public void btnClick(View v) {
 //        EditText edt = findViewById(R.id.edText);
 //        Editable ed = edt.getText();
