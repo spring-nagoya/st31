eaada53cdab57e398fe829d1c8722c15ac03d246
sikigasa <km3t1114@yahoo.co.jp>
2024-07-03 18:52:47 +0900

    add:授業+課題

---
+++ st31_2024_r04_intent/app/src/main/java/com/example/st31_2024_r04_intent/SubActivity.java
@@ -0,0 +1,40 @@
+package com.example.st31_2024_r04_intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sub);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent = getIntent();
+        String intentData =intent.getStringExtra("data1");
+
+        TextView subText = findViewById(R.id.textSub);
+        subText.setText(intentData);
+
+
+
+        Button btnSub = findViewById(R.id.btnSub);
+        btnSub.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
