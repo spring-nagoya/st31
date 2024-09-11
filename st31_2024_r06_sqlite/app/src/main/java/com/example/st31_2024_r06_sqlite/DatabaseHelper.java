4b005789ae876337bb617a833776eba80e1138dc
sikigasa <km3t1114@yahoo.co.jp>
2024-09-11 18:32:47 +0900

    add: 授業

--- st31_2024_r06_sqlite/app/src/main/java/com/example/st31_2024_r06_sqlite/DatabaseHelper.java
+++ st31_2024_r06_sqlite/app/src/main/java/com/example/st31_2024_r06_sqlite/DatabaseHelper.java
@@ -7,23 +7,34 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DB_NAME = "mydSQLite.db";
+    private static final String DB_NAME = "mySQLite.db";
     private static final int DB_VERSION = 1;
-    public DatabaseHelper(
-            @Nullable Context context,
-            @Nullable SQLiteDatabase.CursorFactory factory
-            ) {
-        super(context, DB_NAME, factory, DB_VERSION);
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
     }
+//    public DatabaseHelper(
+//            @Nullable Context context,
+//            @Nullable SQLiteDatabase.CursorFactory factory
+//            ) {
+//        super(context, DB_NAME, factory, DB_VERSION);
+//    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE mytable (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+        String strSQL = "";
+        strSQL = "CREATE TABLE user (id TEXT PRIMARY KEY, name TEXT, age INTEGER, pass TEXT);";
+        db.execSQL(strSQL);
+
+        strSQL = "insert into user values ('00001', '片岡', 43, 'ktok');";
+        db.execSQL(strSQL);
+        strSQL = "insert into user values ('00002', '長屋', 45, 'nagaya');";
+        db.execSQL(strSQL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS mytable");
+        db.execSQL("DROP TABLE IF EXISTS user");
         onCreate(db);
     }
 }
--- st31_2024_r06_sqlite/app/src/main/java/com/example/st31_2024_r06_sqlite/MainActivity.java
+++ st31_2024_r06_sqlite/app/src/main/java/com/example/st31_2024_r06_sqlite/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.st31_2024_r06_sqlite;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -19,34 +24,60 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private DatabaseHelper helper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        EdgeToEdge.enable(this);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_host_fragment_content_main), (v, insets) -> {
+            v.setPadding(v.getPaddingLeft(), v.getPaddingTop(), v.getPaddingRight(), insets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom);
+            return insets;
         });
-    }
+        try {
+            helper = new DatabaseHelper(this);
+        } catch (Exception e) {
+            Snackbar.make(binding.getRoot(), e.getMessage(), Snackbar.LENGTH_LONG).show();
+        }
+        db = helper.getWritableDatabase();
+        Cursor dbRows;
+        dbRows = db.rawQuery("select * from user", null);
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+
+//        dbRows = db.query("t_user",
+//        new String[]{"id","name","age","pass"},
+//        null,
+//        null,
+//        null,
+//        null,
+//        null);
+        dbRows.close();
+        db.close();
+
+//        setSupportActionBar(binding.toolbar);
+
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAnchorView(R.id.fab)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
+
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+//        return NavigationUI.navigateUp(navController, appBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+//}
