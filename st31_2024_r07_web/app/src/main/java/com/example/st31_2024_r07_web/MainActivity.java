94a0f67b662214be6ee7cac6ff72ba899e36ca65
sikigasa <km3t1114@yahoo.co.jp>
2025-01-28 10:17:27 +0900

    add: 授業

---
+++ st31_2024_r07_web/app/src/main/java/com/example/st31_2024_r07_web/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.st31_2024_r07_web;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        WebView webView = findViewById(R.id.webView);
+        WebSettings config = webView.getSettings();
+        config.setJavaScriptEnabled(true);
+        config.setDomStorageEnabled(true);
+//        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://www.google.com");
+
+    }
+}
