9ee46a030746720176e2052b6220cba5c2ca2e2c
sikigasa <km3t1114@yahoo.co.jp>
2025-05-14 17:36:53 +0900

    update: MainActivity

--- 2025_st42_r01_fragment/app/src/main/java/com/example/a2025_st42_r01_fragment/SecondFragment.java
+++ 2025_st42_r01_fragment/app/src/main/java/com/example/a2025_st42_r01_fragment/SecondFragment.java
@@ -58,7 +58,8 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return view;
     }
 }
