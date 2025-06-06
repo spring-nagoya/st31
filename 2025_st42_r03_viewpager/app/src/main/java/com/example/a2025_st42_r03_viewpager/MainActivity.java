1ed77ab5d699caf19a952349cfb2d720b483a8c6
sikigasa <km3t1114@yahoo.co.jp>
2025-06-06 14:33:12 +0900

    add:授業

--- 2025_st42_r03_viewpager/app/src/main/java/com/example/a2025_st42_r03_viewpager/MainActivity.java
+++ 2025_st42_r03_viewpager/app/src/main/java/com/example/a2025_st42_r03_viewpager/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -28,19 +29,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
         this.viewP = findViewById(R.id.viewPager);
-        FragmentStateAdapter adapter;
+        FragmentStateAdapter adapter = new SlidePagerAdapter(this);
         viewP.setAdapter(adapter);
     }
+
     private static class SlidePagerAdapter extends FragmentStateAdapter {
+
+
+        private final int pageNum = 3;
+        public SlidePagerAdapter (@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
 
         @NonNull
         @Override
         public Fragment createFragment(int position) {
-            return null;
+            if (position == 0) {
+                return new FirstFragment();
+            } else if (position == 1) {
+                return new SecondFragment(); // Assuming you have a SecondFragment class
+            } else if (position == 2) {
+                return new ThirdFragment(); // Assuming you have a ThirdFragment class
+            } else {
+                return new FirstFragment(); // Assuming you have a ThirdFragment class
+            }
+
         }
 
         @Override
-        public Fragment
+        public int getItemCount() {
+            return pageNum; // Return the number of pages in the ViewPager
         }
     }
 }
