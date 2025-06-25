f85bfcd255f23fc8ad3740826ba40032a319d43f
sikigasa <km3t1114@yahoo.co.jp>
2025-06-25 14:33:16 +0900

    add:授業

---
+++ 2025_st42_r04_surfaceview/app/src/main/java/com/example/a2025_st42_r04_surfaceview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.a2025_st42_r04_surfaceview;
+
+import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    SurfaceView view = findViewById(R.id.surfaceView);
+    St42SurfaceView stView = new St42SurfaceView(this, view);
+}
